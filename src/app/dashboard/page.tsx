a2fddb9c3fc62f1794b5c69f2d569f713222bef1
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-09-17 03:21:29 +0530

    dashboard - add new

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,15 @@
+
+import { getAllPlaygroundForUser } from "@/module/dashboard/actions";
+import AddNewButton from "@/module/dashboard/components/AddNewButton";
+
 import React from "react";
 
 const Page = async () => {
   return (
     <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
-      playground
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+        <AddNewButton />
+      </div>
     </div>
   );
 };
