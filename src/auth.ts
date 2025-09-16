687ea2bd40f9f7aae7f396c0cfe9a7ceca134d2a
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-09-16 20:17:22 +0530

    signin flow

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,117 @@
-import NextAuth from "next-auth";
+/** biome-ignore-all assist/source/organizeImports: <next-auth> */
+import NextAuth, { Account } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+import { db } from "./lib/db";
+import { getUserById } from "./module/auth/actions";
+import authConfig from "../auth.config";
+
+interface CustomAccount extends Account {
+  session_state: string;
+}
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    /**
+     * Handle user creation and account linking after a successful sign-in
+     */
+    async signIn({ user, account }) {
+      if (!user || !account) return false;
+
+      // Check if the user already exists
+      const existingUser = await db.user.findUnique({
+        where: { email: user.email! },
+      });
+
+      // If user does not exist, create a new one
+      if (!existingUser) {
+        const newUser = await db.user.create({
+          data: {
+            email: user.email!,
+            name: user.name,
+            image: user.image,
+
+            accounts: {
+              create: {
+                type: account.type,
+                provider: account.provider,
+                providerAccountId: account.providerAccountId,
+                refreshToken: account.refresh_token,
+                accessToken: account.access_token,
+                expiresAt: account.expires_at,
+                tokenType: account.token_type,
+                scope: account.scope,
+                idToken: account.id_token,
+                sessionState: (account as CustomAccount).session_state,
+              },
+            },
+          },
+        });
+
+        if (!newUser) return false; // Return false if user creation fails
+      } else {
+        // Link the account if user exists
+        const existingAccount = await db.account.findUnique({
+          where: {
+            provider_providerAccountId: {
+              provider: account.provider,
+              providerAccountId: account.providerAccountId,
+            },
+          },
+        });
+
+        // If the account does not exist, create it
+        if (!existingAccount) {
+          await db.account.create({
+            data: {
+              userId: existingUser.id,
+              type: account.type,
+              provider: account.provider,
+              providerAccountId: account.providerAccountId,
+              refreshToken: account.refresh_token,
+              accessToken: account.access_token,
+              expiresAt: account.expires_at,
+              tokenType: account.token_type,
+              scope: account.scope,
+              idToken: account.id_token,
+              sessionState: (account as CustomAccount).session_state,
+            },
+          });
+        }
+      }
+
+      return true;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      // Attach the user ID from the token to the session
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.sub && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
+
+  secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
---
+++ types/env.d.ts
@@ -0,0 +1,20 @@
+// types/env.d.ts
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV?: "development" | "production" | "test";
+    PORT?: string;
+    CORS_ORIGIN?: string;
+    MONGO_URI?: string;
+    ACCESS_TOKEN_SECRET?: string;
+    ACCESS_EXPIRY?: `${number}d`;
+    REFRESH_TOKEN_SECRET?: string;
+    REFRESH_EXPIRY?: `${number}d`;
+    DATABASE_URL?: string;
+    AUTH_SECRET?: string;
+    AUTH_GITHUB_ID?: string;
+    AUTH_GITHUB_SECRET?: string;
+    AUTH_GOOGLE_ID?: string;
+    AUTH_GOOGLE_SECRET?: string;
+    // Add your other variables here
+  }
+}
