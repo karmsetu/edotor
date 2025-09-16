01cd49a0e392eafa3afe9a8a82378f9c9a6ebeb0
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-09-17 00:21:40 +0530

    home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,11 +28,19 @@ export default async function RootLayout({
   const session = await auth();
   return (
     <SessionProvider session={session}>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+
           {children}
+          </ThemeProvider>
         </body>
       </html>
     </SessionProvider>
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import UserButton from "@/module/auth/components/userButton";
-
-export default function Home() {
-  return (
-    <UserButton/>
-  );
-}
