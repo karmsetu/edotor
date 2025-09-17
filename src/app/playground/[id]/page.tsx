4b8e0860befe68613034022451800ebb8f98ab97
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-09-18 00:00:15 +0530

    playground editor - set active file

--- src/app/playground/[id]/page.tsx
+++ src/app/playground/[id]/page.tsx
@@ -4,24 +4,50 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { TemplateFileTree } from "@/module/playground/components/playGroundExplorer";
+import { useFileExplorer } from "@/module/playground/hooks/useFileExplorer";
 import { usePlayground } from "@/module/playground/hooks/usePlayground";
+import { TemplateFile } from "@/module/playground/lib/pathToJson";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function PlaygroundPage() {
   const { id } = useParams<{ id: string }>();
   const { playgroundData, templateData, isLoading, error, saveTemplateData } =
     usePlayground(id);
-  const activeFile = {
-    filename: "sample",
-    fileExtension: "txt",
-    content: "string",
+
+  const {
+    activeFileId,
+    openFile,
+    closeAllFiles,
+    openFiles,
+    setTemplateData,
+    setOpenFiles,
+    setPlaygroundId,
+    setActiveFileId,
+  } = useFileExplorer();
+
+  useEffect(() => {
+    setPlaygroundId(id);
+  }, [id, setPlaygroundId]);
+
+  useEffect(() => {
+    if (templateData && !openFiles.length) {
+      setTemplateData(templateData);
+    }
+  }, [templateData, setTemplateData, openFiles.length]);
+
+  const activeFile = openFiles.find((file) => file.id === activeFileId);
+  const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
+
+  const handleFileSelect = (file: TemplateFile) => {
+    openFile(file);
   };
 
   return (
     <TooltipProvider>
       <TemplateFileTree
         data={templateData!}
-        onFileSelect={() => {}}
+        onFileSelect={handleFileSelect}
         selectedFile={activeFile}
         title="File Explorer"
         onAddFile={() => {}}
